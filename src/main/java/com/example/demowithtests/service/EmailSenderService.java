9f46b62601a09e9104f76f863e2b7ef5d8ea5e8b
Y'Kotyk <makurohashami@gmail.com>
2023-03-17 11:03:48 +0200

    EmailSenderService delete unused method

--- src/main/java/com/example/demowithtests/service/EmailSenderService.java
+++ src/main/java/com/example/demowithtests/service/EmailSenderService.java
@@ -6,16 +6,15 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor
 @Log4j2
+@AllArgsConstructor
+@Service
 public class EmailSenderService {
 
     private final JavaMailSender mailSender;
 
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("profiler@demoapp.com");
         message.setTo(toEmail);
         message.setSubject(subject);
         message.setText(body);
@@ -24,3 +23,4 @@ public class EmailSenderService {
         log.info("Mail to {} sent successfully", toEmail);
     }
 }
+
