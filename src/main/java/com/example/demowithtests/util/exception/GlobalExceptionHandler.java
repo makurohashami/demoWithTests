b111c810ba23c5391a7a43ef59984bff706096c4
Y'Kotyk <makurohashami@gmail.com>
2023-03-19 22:00:35 +0200

    delete unused exception ResourceWasDeletedException

--- src/main/java/com/example/demowithtests/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demowithtests/util/exception/GlobalExceptionHandler.java
@@ -26,11 +26,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(ResourceWasDeletedException.class)
-    protected ResponseEntity<MyGlobalExceptionHandler> handleDeleteException() {
-        return new ResponseEntity<>(new MyGlobalExceptionHandler("This user was deleted"), HttpStatus.NOT_FOUND);
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
