2164399f7fb33f4916e7700abb1e876f58979018
Y'Kotyk <makurohashami@gmail.com>
2023-03-19 15:37:09 +0200

    renamed class AppConfig.java to MailConfig.java

--- src/main/java/com/example/demowithtests/util/config/MailConfig.java
+++ src/main/java/com/example/demowithtests/util/config/MailConfig.java
@@ -8,7 +8,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-public class AppConfig {
+public class MailConfig {
 
     @Bean
     public JavaMailSender getJavaMailSender() {
