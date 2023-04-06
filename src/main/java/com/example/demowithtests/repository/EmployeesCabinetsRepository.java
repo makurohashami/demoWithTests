da4a8cfe9009061aaaac376cf73f8cd987de7ded
Y'Kotyk <makurohashami@gmail.com>
2023-04-07 00:26:37 +0300

    mark EmployeesCabinetsRepository as Repository

--- src/main/java/com/example/demowithtests/repository/EmployeesCabinetsRepository.java
+++ src/main/java/com/example/demowithtests/repository/EmployeesCabinetsRepository.java
@@ -3,7 +3,9 @@ package com.example.demowithtests.repository;
 import com.example.demowithtests.domain.EmployeesCabinets;
 import com.example.demowithtests.domain.EmployeesCabinetsKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeesCabinetsRepository extends JpaRepository<EmployeesCabinets, EmployeesCabinetsKey> {
 
 }
